354064616f778a9e4379a7f797479f2dfba8472e
NicoHerlan <nicodvkcss@gmail.com>
2023-12-10 13:27:08 -0300

    Agregar archivos de proyecto.

---
+++ WhatsAppCloneBackend/Program.cs
@@ -0,0 +1,47 @@
+using WhatsAppCloneBackend.Hubs;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("CorsPolicy", builder =>
+    {
+        builder
+            .SetIsOriginAllowed(origin => origin.StartsWith("http://localhost"))
+            .AllowAnyHeader()
+            .AllowAnyMethod()
+            .AllowCredentials();
+    });
+});
+
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+// Add Signal R Service
+builder.Services.AddSignalR();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseCors("CorsPolicy");
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.MapHub<ChatHub>("/chatHub");
+
+app.Run();
