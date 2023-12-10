230df7bba68512ce1fb6f89af4ae076922e30681
NicoHerlan <nicodvkcss@gmail.com>
2023-12-10 15:15:24 -0300

    change cors config

--- WhatsAppCloneBackend/Program.cs
+++ WhatsAppCloneBackend/Program.cs
@@ -11,7 +11,7 @@ builder.Services.AddCors(options =>
     options.AddPolicy("CorsPolicy", builder =>
     {
         builder
-            .SetIsOriginAllowed(origin => origin.StartsWith("http://localhost"))
+            .SetIsOriginAllowed(origin => true)
             .AllowAnyHeader()
             .AllowAnyMethod()
             .AllowCredentials();
